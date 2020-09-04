aad570f0d32be7e4ddd4ccb6dae16faf1f6e8aeb
tam <tam.vm@aris-vn.com>
2020-09-04 17:18:15 +0700

    4/9/2020 17:18

--- PlayFilemp3/Mode/database.swift
+++ PlayFilemp3/Mode/database.swift
@@ -7,9 +7,10 @@
 //
 
 import Foundation
+import  RealmSwift
 
-class database {
-    static var shareInstance = database()
+class DatabaseManager {
+    
 //    var arrDB: [databaseInital] = [
 //                                    databaseInital(name: "049 Track049", text: "who is in charge of purchasing office supplies?* Alice supplies the coffee.* That would be our office manager, Alice. * The supplies are in the main office.*", audio: "0.80-0.11* 0.14-0.1*0.19-0.22*0.24-0.27*"),
 //                                    databaseInital(name: "050 Track050", text: "which catering company did we hire for the banquet? *The one we used last year. *Just a few waiters. *A large ballroom.*", audio: ""),
@@ -20,7 +21,68 @@ class database {
 //                                    databaseInital(name: "055 Track055", text: "why did Ms.Anderson not sign the contract?*She hasn’t finished yet.*I didn’t see the contract.*She never gave a reason.*", audio: ""),
 //                                    databaseInital(name: "Test 01", text: "why did Ms.Anderson not sign the contract?*She hasn’t finished yet.*I didn’t see the contract.*She never gave a reason.*", audio: "")
 //                                ]
+    var arrDB: Results<DatabaseInital>!
     
-    var arrDB: [DatabaseInital] = []
+    private var database: Realm
+
+    static var shareInstance = DatabaseManager()
+    
+    private init() {
+        database = try! Realm()
+        
+    }
+    
+    func getDataRealmIntial() -> Results<DatabaseInital>{
+         let result: Results<DatabaseInital> = database.objects(DatabaseInital.self)
+        return result
+     }
+    
+    func getDataFromDB() -> Bool {
+        let result: Results<DatabaseInital> = database.objects(DatabaseInital.self)
+        if result.count == 0 {
+            return false
+        }else {
+             arrDB = result
+            return true
+        }
+       
+    }
+    
+    func addData(url: String, name: String, text: String, audio: String) {
+        let object = DatabaseInital()
+        object.url = url
+        object.name = name
+        object.text = text
+        object.audio = audio
+        try! database.write{
+            database.add(object)
+        }
+    }
+    
+    func updateToDB(object: DatabaseInital) -> Bool {
+        let result = database.objects(DatabaseInital.self).filter("name = %@", object.name)
+        do {
+            try database.write{
+                result.first?.name = object.name
+                result.first?.audio = object.audio
+                result.first?.text = object.text
+                result.first?.url = object.url
+            }
+            return true
+        }catch {
+            return false
+        }
+    }
+    
+    func deleteAllFromDB() -> Bool {
+        do {
+            try database.write{
+                database.deleteAll()
+            }
+            return true
+        }catch {
+            return false
+        }
+    }
     
 }
