337aabdf225aae7d98dbcb95b4b4f3edd01b0c09
tam <tam.vm@aris-vn.com>
2020-09-04 08:34:01 +0700

    4/9/2020 08:33

--- PlayFilemp3/Mode/database.swift
+++ PlayFilemp3/Mode/database.swift
@@ -9,14 +9,18 @@
 import Foundation
 
 class database {
-    var arrDB: [databaseInital] = [
-                                    databaseInital(name: "049 Track049", text: "who is in charge of purchasing office supplies?* Alice supplies the coffee.* That would be our office manager, Alice. * The supplies are in the main office.*", audio: "0.80-0.11* 0.14-0.1*0.19-0.22*0.24-0.27*"),
-                                    databaseInital(name: "050 Track050", text: "which catering company did we hire for the banquet? *The one we used last year. *Just a few waiters. *A large ballroom.*", audio: ""),
-                                    databaseInital(name: "051 Track051", text: "when did Johansson say the meeting was? *He didn’t say what time. *He did an excellent job. *The meeting was cut short.*", audio: ""),
-                                    databaseInital(name: "052 Track052", text: "How often is the office cleaned? *I’ll do it tonight.*No, she’s out today.*At least once a week.*", audio: ""),
-                                    databaseInital(name: "053 Track053", text: "where can I ask for directions? *At the information booth.*That’s why I brought a map.*I never noticed that sign.*", audio: ""),
-                                    databaseInital(name: "054 Track054", text: "Whose car is this?*This is bigger than we need.*Bobby’s, from accounting.*I bought the car.*", audio: ""),
-                                    databaseInital(name: "055 Track055", text: "why did Ms.Anderson not sign the contract?*She hasn’t finished yet.*I didn’t see the contract.*She never gave a reason.*", audio: ""),
-                                    databaseInital(name: "Test 01", text: "why did Ms.Anderson not sign the contract?*She hasn’t finished yet.*I didn’t see the contract.*She never gave a reason.*", audio: "")
-                                ]
+    static var shareInstance = database()
+//    var arrDB: [databaseInital] = [
+//                                    databaseInital(name: "049 Track049", text: "who is in charge of purchasing office supplies?* Alice supplies the coffee.* That would be our office manager, Alice. * The supplies are in the main office.*", audio: "0.80-0.11* 0.14-0.1*0.19-0.22*0.24-0.27*"),
+//                                    databaseInital(name: "050 Track050", text: "which catering company did we hire for the banquet? *The one we used last year. *Just a few waiters. *A large ballroom.*", audio: ""),
+//                                    databaseInital(name: "051 Track051", text: "when did Johansson say the meeting was? *He didn’t say what time. *He did an excellent job. *The meeting was cut short.*", audio: ""),
+//                                    databaseInital(name: "052 Track052", text: "How often is the office cleaned? *I’ll do it tonight.*No, she’s out today.*At least once a week.*", audio: ""),
+//                                    databaseInital(name: "053 Track053", text: "where can I ask for directions? *At the information booth.*That’s why I brought a map.*I never noticed that sign.*", audio: ""),
+//                                    databaseInital(name: "054 Track054", text: "Whose car is this?*This is bigger than we need.*Bobby’s, from accounting.*I bought the car.*", audio: ""),
+//                                    databaseInital(name: "055 Track055", text: "why did Ms.Anderson not sign the contract?*She hasn’t finished yet.*I didn’t see the contract.*She never gave a reason.*", audio: ""),
+//                                    databaseInital(name: "Test 01", text: "why did Ms.Anderson not sign the contract?*She hasn’t finished yet.*I didn’t see the contract.*She never gave a reason.*", audio: "")
+//                                ]
+    
+    var arrDB: [DatabaseInital] = []
+    
 }
