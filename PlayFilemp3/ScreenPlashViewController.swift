e2fee7996a428535062b37f8411372eaf02a4612
tam <tam.vm@aris-vn.com>
2020-09-14 13:44:41 +0700

    14/9/2020 13:44

--- PlayFilemp3/ScreenPlashViewController.swift
+++ PlayFilemp3/ScreenPlashViewController.swift
@@ -13,6 +13,15 @@ class ScreenPlashViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+//        // https://dictionary.cambridge.org/vi/media/english/uk_pron/u/ukf/ukfau/ukfault029.mp3
+//        // https://dictionary.cambridge.org/vi/media/english/uk_pron/u/uki/uking/ukingra015.mp3
+//        //https://dictionary.cambridge.org/vi/media/english/uk_pron/u/ukm/ukmes/ukmesh_001.mp3
+        
+//        guard let url = URL(string: "https://dictionary.cambridge.org/vi/media/english/uk_pron/u/ukf/ukfau/ukfault029.mp3") else {return}
+//        let urlSession = URLSession(configuration: .default, delegate: self, delegateQueue: OperationQueue())
+//        let downloadTask = urlSession.downloadTask(with: url)
+//        downloadTask.resume()
+        
         if UserDefaults.standard.bool(forKey: "createFileTXT") == false {
             createFileTXT()
         }
@@ -42,3 +51,25 @@ class ScreenPlashViewController: UIViewController {
     }
 
 }
+
+//extension ScreenPlashViewController: URLSessionDownloadDelegate {
+//    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+//        print("File Download", location)
+//        guard let url = downloadTask.originalRequest?.url else {
+//            return
+//        }
+////        let docsPath = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+//        let docsPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+//        let destinationPath = docsPath.appendingPathComponent(url.lastPathComponent)
+//        
+//        try? FileManager.default.removeItem(at: destinationPath)
+//        
+//        do {
+//            try FileManager.default.copyItem(at: location, to: destinationPath)
+//
+//        }catch {
+//            print("copy Error : \(error.localizedDescription)")
+//        }
+//        
+//    }
+//}
